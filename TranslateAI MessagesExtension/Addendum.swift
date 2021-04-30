5b6cdb353e60b2e8987f91a26c638754e54cfcf1
Jonathan Xu <s014290@students.lmsd.org>
2021-04-30 09:17:59 -0400

    jjk

---
+++ TranslateAI MessagesExtension/Addendum.swift
@@ -0,0 +1,40 @@
+//
+//  Addendum.swift
+//  TranslateAI MessagesExtension
+//
+//  Created by Jonathan Xu (student LM) on 4/30/21.
+//  Copyright © 2021 Eric Yang (student LM). All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
