3ef6b2dde563d070613aa388fd168ddeb7407637
Hologram-guy <77369752+Hologram-guy@users.noreply.github.com>
2023-08-17 00:38:03 -0400

    clean up GTAlertCollection.swift

--- TranslateAI MessagesExtension/GTAlertCollection.swift
+++ TranslateAI MessagesExtension/GTAlertCollection.swift
@@ -1,11 +1,6 @@
 //
 //  GTAlertCollection.swift
-//
-//  Created by Gabriel Theodoropoulos.
-//
-//  MIT License
-//  Copyright © 2018 Gabriel Theodoropoulos
-//
+
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
@@ -13,16 +8,6 @@
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
 //
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
 
 import UIKit
 
@@ -39,7 +24,6 @@ import UIKit
 /// *   Alert controller with multiple text fields
 /// *   Alert controller with an image view
 ///
-/// See more on [GitHub](https://github.com/gabrieltheodoropoulos/gtalertcollection)
 class GTAlertCollection: NSObject {
 
     // MARK: - Properties
